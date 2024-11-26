a42867462ad3ed205b1e5f02532e1fdfad312b74
Jerez <41114425+mateusjerez@users.noreply.github.com>
2024-11-25 21:51:19 -0300

    fix problems packages

--- src/server.js
+++ src/server.js
@@ -1,21 +1,20 @@
 
 import express from "express";
 import jsonwebtoken from "jsonwebtoken";
-import { tokenValited } from "./auth";
-import { PRIVATE_KEY, user } from "./auth.js";
+import { PRIVATE_KEY, tokenValited, user } from "./auth.js";
 
 const api = express();
 api.use(express.json());
 
 api.get("/", (_, res) => res.status(200).json({ message: "This is a public router..." }));
 
-api.get("/login", (req, res) => {
+api.post("/login", (req, res) => {
     const [, hash] = req.headers.authorization?.split(" ") || [' ', ' '];
     const [email, password] = Buffer.from(hash, "base64").toString().split(":");
 
     try{
-        const correctPassword = email === "teste@qualquer.com" && password === "123456";
-
+        const correctPassword = email === "teste@qualquer.com" && password === "123456"; //usuário e senha para exemplo.. 
+        console.log(email, password);
         if(!correctPassword) return res.status(401).send("Access denied. Invalid credentials");
 
         const token = jsonwebtoken.sign(
@@ -28,19 +27,18 @@ api.get("/login", (req, res) => {
         console.log(err);
         return res.send(err);
     }
-
-    api.use('*', tokenValited)
-
-    api.get("/private", (req, res) => {
-        const { user } = req.headers
-        const currentUser = JSON.parse(user);
-        return res.status(200).json({
-            message: `This is a private router. Welcome ${currentUser.name}`,
-            data: {
-                userLogged: currentUser
-            }
-        })
-    })
-
-    api.listen(3000, () => console.log("Server is running on port 3000..."));
 })
+api.use('*', tokenValited)
+
+api.get("/private", (req, res) => {
+    const { user } = req.headers
+    const currentUser = JSON.parse(user);
+    return res.status(200).json({
+        message: `This is a private router. Welcome ${currentUser.name}`,
+        data: {
+            userLogged: currentUser
+        }
+    })
+})
+
+api.listen(3000, () => console.log("Server is running on port 3000..."));
